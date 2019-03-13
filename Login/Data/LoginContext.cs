894a8bfa0de70d9226fdf372ad288fe14e79c815
Cristhian Antonio Alcalá <31580380+CantonioA@users.noreply.github.com>
2019-03-12 23:59:23 -0500

    --

---
+++ Login/Data/LoginContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Login.Models
+{
+    public class LoginContext : DbContext
+    {
+        public LoginContext (DbContextOptions<LoginContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Login.Models.Persona> Persona { get; set; }
+    }
+}
---
+++ Login/Models/Persona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login.Models
+{
+    public class Persona
+    {
+        [Key]
+        public string DNIPER { get; set; }
+        public string NOMPER { get; set; }
+        public string APEPER { get; set; }
+        public string USUPER { get; set; }
+        public string CONPER { get; set; }
+        public int NIVPER { get; set; }
+    }
+}
